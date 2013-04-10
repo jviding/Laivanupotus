d6b3cc9fcb51d9891855c48d1c70e75e750c4d81
jviding <jasu.viding@helsinki.fi>
2013-04-10 03:49:31 +0300

    Testit

--- test/NewLuoPelikenttaJUnitTest.java
+++ test/NewLuoPelikenttaJUnitTest.java
@@ -1,23 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
-/**
- *
- * @author Timo
- */
 public class NewLuoPelikenttaJUnitTest {
 
     public NewLuoPelikenttaJUnitTest() {
     }
+
+    LuoPelikentta pieni = new LuoPelikentta();
+    LuoPelikentta keskikok = new LuoPelikentta();
+    LuoPelikentta iso = new LuoPelikentta();
+
 
     @BeforeClass
     public static void setUpClass() throws Exception {
@@ -29,16 +24,65 @@ public class NewLuoPelikenttaJUnitTest {
 
     @Before
     public void setUp() {
+        pieni.luoKentta(1);
+        keskikok.luoKentta(2);
+        iso.luoKentta(3);
     }
 
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 
+    @Test
+    public void kenttienMitatTest() {
+        assertTrue(pieni.kentanKanta==8*1);
+        assertTrue(keskikok.kentanKanta==8*2);
+        assertTrue(iso.kentanKanta==8*3);
+    }
+    @Test
+    public void sijoitetaanPystylaivatTest(){
+        assertTrue(pieni.mahtuukoPystyna(3,1,1));
+        assertTrue(keskikok.mahtuukoPystyna(3,1,1));
+        assertTrue(iso.mahtuukoPystyna(3,1,1));
+        assertFalse(pieni.mahtuukoPystyna(8*1+1,1,1));
+        assertFalse(keskikok.mahtuukoPystyna(8*2+1,1,1));
+        assertFalse(iso.mahtuukoPystyna(8*3+1,1,1));
+    }
+    @Test
+    public void sijoitetaanVaakalaivatTest(){
+        assertTrue(pieni.mahtuukoVaakana(4,1,2));
+        assertTrue(keskikok.mahtuukoVaakana(4,1,2));
+        assertTrue(iso.mahtuukoVaakana(4,1,2));
+        assertFalse(pieni.mahtuukoVaakana(8*1+1,1,1));
+        assertFalse(keskikok.mahtuukoVaakana(8*2+1,1,1));
+        assertFalse(iso.mahtuukoVaakana(8*3+1,1,1));
+    }
+    @Test
+    public void onkoYmparillaTestFalse(){
+        assertFalse(pieni.onkoYmparilla(4,8));
+        assertFalse(keskikok.onkoYmparilla(16,7));
+        assertFalse(iso.onkoYmparilla(24,24));
+    }
+    @Test
+    public void onkoYmparillaTestTrue(){
+        pieni.moneskoLaiva=3;
+        keskikok.moneskoLaiva=2;
+        iso.moneskoLaiva=5;
+        pieni.mahtuukoVaakana(4,1,2);
+        keskikok.mahtuukoPystyna(4,1,2);
+        iso.mahtuukoVaakana(4,1,2);
+        assertTrue(pieni.onkoYmparilla(2,3));
+        assertTrue(keskikok.onkoYmparilla(1,1));
+        assertTrue(iso.onkoYmparilla(1,2));
+    }
+    @Test
+    public void laivamaaratTest(){
+        pieni.asetaLaivat(1);
+        keskikok.asetaLaivat(2);
+        iso.asetaLaivat(3);
+        assertTrue(pieni.moneskoLaiva==(1*(1+2+3)));
+        assertTrue(keskikok.moneskoLaiva==(2*(1+2+3)));
+        assertTrue(iso.moneskoLaiva==(3*(1+2+3)));
+    }
 }
--- test/NewPelaaJUnitTest.java
+++ test/NewPelaaJUnitTest.java
@@ -1,23 +1,17 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
-/**
- *
- * @author Timo
- */
 public class NewPelaaJUnitTest {
 
     public NewPelaaJUnitTest() {
     }
+
+    Pelaa pieniPeli = new Pelaa(1);
+    Pelaa keskikokPeli = new Pelaa(2);
+    Pelaa isoPeli = new Pelaa(3);
 
     @BeforeClass
     public static void setUpClass() throws Exception {
@@ -29,16 +23,58 @@ public class NewPelaaJUnitTest {
 
     @Before
     public void setUp() {
+        LuoPelikentta pelikentta = new LuoPelikentta();
+        pelikentta.luoKentta(1);
+        pieniPeli.kentta=pelikentta.kentta;
+        pelikentta.luoKentta(2);
+        keskikokPeli.kentta=pelikentta.kentta;
+        pelikentta.luoKentta(3);
+        isoPeli.kentta=pelikentta.kentta;
     }
 
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 
+    @Test
+    public void osukoTestFalse() {
+        assertFalse(pieniPeli.osuko(3,6));
+        assertFalse(keskikokPeli.osuko(1,9));
+        assertFalse(isoPeli.osuko(15,3));
+    }
+    @Test
+    public void osukoTestTrue(){
+        pieniPeli.kentta[1][2]=1;
+        keskikokPeli.kentta[3][7]=8;
+        isoPeli.kentta[10][12]=3;
+        assertTrue(pieniPeli.osuko(1,2));
+        assertTrue(keskikokPeli.osuko(3,7));
+        assertTrue(isoPeli.osuko(10,12));
+    }
+    @Test
+    public void onkoJaljellaTest(){
+        int[] esimTilanne = new int[10];
+        pieniPeli.laivojenTilanne=esimTilanne;
+        assertFalse(pieniPeli.onkoJaljella());
+        esimTilanne[3]=3;
+        keskikokPeli.laivojenTilanne=esimTilanne;
+        assertTrue(keskikokPeli.onkoJaljella());
+        esimTilanne[1]=4;
+        isoPeli.laivojenTilanne=esimTilanne;
+        assertTrue(isoPeli.onkoJaljella());
+    }
+    @Test
+    public void upposikoTest(){
+        int[] esimTilanne = new int[10];
+        esimTilanne[1]=1;
+        pieniPeli.laivojenTilanne=esimTilanne;
+        assertTrue(pieniPeli.upposiko(1));
+        esimTilanne[3]=3;
+        keskikokPeli.laivojenTilanne=esimTilanne;
+        assertFalse(keskikokPeli.upposiko(3));
+        esimTilanne[1]=4;
+        isoPeli.laivojenTilanne=esimTilanne;
+        assertFalse(isoPeli.upposiko(1));
+    }
 }
